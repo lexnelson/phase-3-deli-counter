927572058590c9cc6820067a66ca540963ac29f5
Lex Nelson <lexinelson597@gmail.com>
2021-08-09 10:39:45 -0400

    commit tree be91c0ba7684080e21d3220ba1cfd7f7cec1f583

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require 'pry'
+katz_deli = []
+
+def line(deli)
+    if deli.empty?
+       puts "The line is currently empty."
+    else 
+        resp = "The line is currently:"
+        deli.each.with_index(1) do |cust, i|
+             resp << " #{i}. #{cust}"
+        end
+        puts resp
+    end
+end
+
+def take_a_number(deli, name)
+    deli.push(name)
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+    if deli.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{deli[0]}."
+        deli.shift()
+    end
+end
